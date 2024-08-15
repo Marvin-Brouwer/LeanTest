5363cf6bf3b915a7e69990586b95f2da5b03efec
Marvin Brouwer <marvin-brouwer@live.nl>
2024-08-15 21:29:07 +0200

    feat: Removed duplicate attributes

--- src/LeanTest.TestAdapter/TestDiscoverer.cs
+++ src/LeanTest.TestAdapter/TestDiscoverer.cs
@@ -5,7 +5,5 @@ using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 namespace LeanTest.TestAdapter;
 
 [DefaultExecutorUri(VsTestExecutor.Id)]
-// TODO see if this can be left out vv
-[FileExtension(".exe"), FileExtension(".dll")]
 internal sealed class TestDiscoverer
 	: VsTestDiscoverer;
